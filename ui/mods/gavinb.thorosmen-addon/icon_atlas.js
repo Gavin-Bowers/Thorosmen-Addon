2713fdf175199fe65bb6d80e07c98090e2b1f479
Gavin Bowers <gbowersformal@gmail.com>
2023-07-19 23:50:38 -0700

    reverted change

--- ui/mods/gavinb.thorosmen-addon/icon_atlas.js
+++ ui/mods/gavinb.thorosmen-addon/icon_atlas.js
@@ -1,6 +1,5 @@
 var paeiouIcons = [ 
     "fefkins",
     "mini-fef",
-    "land_barier_big"
 ];
 model.strategicIcons(model.strategicIcons().concat(paeiouIcons));
--- ui/mods/gavinb.thorosmen-addon/shared_build.js
+++ ui/mods/gavinb.thorosmen-addon/shared_build.js
@@ -1,6 +1,5 @@
 var newBuild = {
     "/pa/units/th-addon/fefkins/fefkins.json": ["combat", 0, { row: 0, column: 6 }],
-    "/pa/units/th-addon/land_barrier_big/land_barrier_big.json": ["utility", 0, { row: 1, column: 0 }]
 }
 if (Build && Build.HotkeyModel && Build.HotkeyModel.SpecIdToGridMap) {
     _.extend(Build.HotkeyModel.SpecIdToGridMap, newBuild);

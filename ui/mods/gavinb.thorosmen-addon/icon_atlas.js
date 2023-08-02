fbce94407cb1e428dc3f6a17a7ec567049d552b8
Gavin Bowers <125422388+Gavin-Bowers@users.noreply.github.com>
2023-08-01 20:20:57 -0700

    Improved big wall model and texture

--- ui/mods/gavinb.thorosmen-addon/icon_atlas.js
+++ ui/mods/gavinb.thorosmen-addon/icon_atlas.js
@@ -1,6 +1,7 @@
 var paeiouIcons = [ 
     "fefkins",
     "mini-fef",
-    "land_wall_big"
+    "land_wall_big",
+    "spy_sat"
 ];
 model.strategicIcons(model.strategicIcons().concat(paeiouIcons));
--- ui/mods/gavinb.thorosmen-addon/shared_build.js
+++ ui/mods/gavinb.thorosmen-addon/shared_build.js
@@ -1,6 +1,7 @@
 var newBuild = {
     "/pa/units/th-addon/fefkins/fefkins.json": ["combat", 0, { row: 0, column: 6 }],
-    "/pa/units/th-addon/land_wall_big/land_wall_big.json": ["utility", 0, { row: 1, column: 0 }]
+    "/pa/units/th-addon/land_wall_big/land_wall_big.json": ["utility", 0, { row: 1, column: 0 }],
+    "/pa/units/th-addon/spy_sat/spy_sat.json": ["orbital", 0, { row: 1, column: 4 }]
 }
 if (Build && Build.HotkeyModel && Build.HotkeyModel.SpecIdToGridMap) {
     _.extend(Build.HotkeyModel.SpecIdToGridMap, newBuild);
